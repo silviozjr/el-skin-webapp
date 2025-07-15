7e58f8cd022aaae6b7f1d45252f32cf50f08d024
Silvio Zimmermann Jr <szjunior@mppr.mp.br>
2025-07-15 12:02:04 -0300

    Adiciona carousel

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import Header from "../../components/Header/Header";
+import Carousel from '../../components/Carousel/Carousel';
 
 export default function Home() {
     return (
-        <Header/>
+        <>
+            <Header/>
+            <Carousel/>
+        </>
     )
 }
