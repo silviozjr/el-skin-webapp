be8e6a535156f5006cc9931a2ed80f9f8b6ed3f1
Silvio Zimmermann Jr <szjunior@mppr.mp.br>
2025-07-16 12:24:47 -0300

    Adiciona footer

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from "../../components/Header/Header";
 import Carousel from '../../components/Carousel/Carousel';
 import ProductGrid from '../../components/ProductGrid/ProductGrid';
+import Footer from '../../components/Footer/Footer';
 
 export default function Home() {
     return (
@@ -9,6 +10,7 @@ export default function Home() {
             <Header/>
             <Carousel/>
             <ProductGrid/>
+            <Footer/>
         </>
     )
 }
