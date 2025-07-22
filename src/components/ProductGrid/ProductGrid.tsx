cdd6e41b0590ac365fd610669f0c7765f2480c94
Silvio Zimmermann Jr <szjunior@mppr.mp.br>
2025-07-22 14:50:26 -0300

    Carrega produtos da API

--- src/components/ProductGrid/ProductGrid.tsx
+++ src/components/ProductGrid/ProductGrid.tsx
@@ -1,18 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './ProductGrid.css';
-import Prod1 from '../../assets/prod1.jpg';
-import Prod2 from '../../assets/prod2.jpg';
-import Prod3 from '../../assets/prod3.jpg';
-import Prod4 from '../../assets/prod4.jpg';
-import Prod5 from '../../assets/prod5.jpg';
-import Prod6 from '../../assets/prod6.jpg';
-import Prod7 from '../../assets/prod7.jpg';
-import Prod8 from '../../assets/prod8.jpg';
 import ProductCard, { IProduto } from "../ProductCard/ProductCard";
+import axios from "axios";
 
 
 
 function ProductGrid() {
+
+  const [produtos, setProdutos] = useState<IProduto[]>([])
 
   const handleProductClick = (productId: string) => {
     console.log(`Produto clicado: ${productId}`);
@@ -23,96 +18,15 @@ function ProductGrid() {
     console.log(`Comprar produto: ${productId}`);
   };
 
-  const produtos: IProduto[] = [
-    {
-      id: '1',
-      nome: 'Protetor solar AL SUN',
-      descricao: 'alta proteção e pele luminosa sem grude nem pele cinzenta',
-      preco: 79.90,
-      imagem: Prod1,
-      tags: [
-        { label: 'proteção', tipo: 'protection' },
-        { label: 'rosto', tipo: 'face' }
-      ]
-    },
-    {
-      id: '2',
-      nome: 'Protetor solar AL SUN',
-      descricao: 'alta proteção e pele luminosa sem grude nem pele cinzenta',
-      preco: 79.90,
-      imagem: Prod2,
-      tags: [
-        { label: 'proteção', tipo: 'protection' },
-        { label: 'rosto', tipo: 'face' }
-      ]
-    },
-    {
-      id: '3',
-      nome: 'Protetor solar AL SUN',
-      descricao: 'alta proteção e pele luminosa sem grude nem pele cinzenta',
-      preco: 79.90,
-      imagem: Prod3,
-      tags: [
-        { label: 'proteção', tipo: 'protection' },
-        { label: 'rosto', tipo: 'face' }
-      ]
-    },
-    {
-      id: '4',
-      nome: 'Protetor solar AL SUN',
-      descricao: 'alta proteção e pele luminosa sem grude nem pele cinzenta',
-      preco: 79.90,
-      imagem: Prod4,
-      tags: [
-        { label: 'proteção', tipo: 'protection' },
-        { label: 'rosto', tipo: 'face' }
-      ]
-    },
-    {
-      id: '5',
-      nome: 'Protetor solar AL SUN',
-      descricao: 'alta proteção e pele luminosa sem grude nem pele cinzenta',
-      preco: 79.90,
-      imagem: Prod5,
-      tags: [
-        { label: 'proteção', tipo: 'protection' },
-        { label: 'rosto', tipo: 'face' }
-      ]
-    },
-    {
-      id: '6',
-      nome: 'Protetor solar AL SUN',
-      descricao: 'alta proteção e pele luminosa sem grude nem pele cinzenta',
-      preco: 79.90,
-      imagem: Prod6,
-      tags: [
-        { label: 'proteção', tipo: 'protection' },
-        { label: 'rosto', tipo: 'face' }
-      ]
-    },
-    {
-      id: '7',
-      nome: 'Protetor solar AL SUN',
-      descricao: 'alta proteção e pele luminosa sem grude nem pele cinzenta',
-      preco: 79.90,
-      imagem: Prod7,
-      tags: [
-        { label: 'proteção', tipo: 'protection' },
-        { label: 'rosto', tipo: 'face' }
-      ]
-    },
-    {
-      id: '8',
-      nome: 'Protetor solar AL SUN',
-      descricao: 'alta proteção e pele luminosa sem grude nem pele cinzenta',
-      preco: 79.90,
-      imagem: Prod8,
-      tags: [
-        { label: 'proteção', tipo: 'protection' },
-        { label: 'rosto', tipo: 'face' }
-      ]
-    },
-  ]
+
+  useEffect(() => {
+    async function buscarProdutos() {
+      axios.get('http://localhost:3001/products')
+        .then(response => setProdutos(response.data));
+    }
+
+    buscarProdutos();
+  }, []);
 
   return (
     <section className="product-grid-section">
