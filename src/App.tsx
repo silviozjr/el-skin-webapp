737fd654233a0a76ebfa30ed10cd06e98783993a
Silvio Zimmermann Jr <szjunior@mppr.mp.br>
2025-07-31 12:16:27 -0300

    Styled components, GlobalStyles e Sobre

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import './App.css';
 import AppRouter from './routes';
 import { SearchProvider } from './contexts/SearchContext';
 import { CartProvider } from './contexts/CartContext';
+import GlobalStyles from './components/GlobalStyles/GlobalStyles';
 
 function App() {
   return (
     <CartProvider>
       <SearchProvider>
+        <GlobalStyles />
         <AppRouter />
       </SearchProvider>
     </CartProvider>
